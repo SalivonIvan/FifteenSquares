b19ce321a89cca4ccc945f0c86df848774051c32
ivan_salivon <isalivon@datas_tech.com>
2018-03-16 17:49:38 +0200

    add music move

--- src/main/java/com/fifteen/squares/Setting.java
+++ src/main/java/com/fifteen/squares/Setting.java
@@ -1,9 +1,12 @@
 package com.fifteen.squares;
 
+import com.gluonhq.charm.down.Platform;
 import com.gluonhq.charm.down.Services;
 import com.gluonhq.charm.down.plugins.VideoService;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+
+import java.util.Optional;
 
 /**
  * Created by ivan salivon on 15.03.18.
@@ -18,38 +21,40 @@ public class Setting {
     public static final boolean VIBRATION = false;
 
     private static MediaPlayer mp;
+    private static Optional<VideoService> vs;
 
     private Setting() {
     }
 
     public static void settingFoneMusic() {
 
-//        if (mp == null) {
-//            System.out.println("PLAY_FONE1");
-//            Media foneMusic = new Media(Setting.class.getResource("fone.mp3").toString());
-//            mp = new MediaPlayer(foneMusic);
-//            mp.setCycleCount(MediaPlayer.INDEFINITE);
-//            mp.setVolume(DEFAULT_SOUND_VOLUME);
-//            System.out.println("PLAY_FONE2");
-//        }
-        if (mp == null) {
-            System.out.println("PLAY_FONE1");
-            Services.get(VideoService.class).ifPresent(service -> {
-                service.setControlsVisible(true);
-                service.getPlaylist().add(Setting.class.getClassLoader().getResource("/com/fifteen/squares/fone.mp3").toString());
-                service.play();
-                System.out.println("PLAY_FONE2");
+        if (Platform.isDesktop()) {
+            if (mp == null) {
+                Media foneMusic = new Media(Setting.class.getResource("fone.mp3").toString());
+                mp = new MediaPlayer(foneMusic);
+                mp.setCycleCount(MediaPlayer.INDEFINITE);
+                mp.setVolume(DEFAULT_SOUND_VOLUME);
+            }
+            if (SOUND)
+                mp.setAutoPlay(true);
+
+        }
+        if (Platform.isAndroid()) {
+            if (vs == null)
+                vs = Services.get(VideoService.class);
+
+            vs.ifPresent(service -> {
+
+                if (SOUND) {
+                    service.getPlaylist().add("com/fifteen/squares/fone.mp3");
+                    service.setLooping(true);
+                    service.play();
+                }
+
             });
-//            System.out.println("PLAY_FONE1");
-//            Media foneMusic = new Media(Setting.class.getResource("fone.mp3").toString());
-//            mp = new MediaPlayer(foneMusic);
-//            mp.setCycleCount(MediaPlayer.INDEFINITE);
-//            mp.setVolume(DEFAULT_SOUND_VOLUME);
-//            System.out.println("PLAY_FONE2");
+
         }
 
-//        if (SOUND)
-//            mp.setAutoPlay(true);
-        System.out.println("PLAY_FONE3");
+
     }
 }
